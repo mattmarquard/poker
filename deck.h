b010a04f2cfe4e0e0977f5a340565f25b11b2639
Matthew MacLennan <matt@thevertigo.com>
2014-02-16 00:49:20 -0800

    Update deck.h

--- deck.h
+++ deck.h
@@ -1,17 +1,22 @@
 //guard
 #ifndef __POKER_H_INCLUDED__
 #define __POKER_H_INCLUDED__
+#include <vector>
 #include "hand.h"
 
 #define NUMINDECK 52
 
-class PokerDeck 
+class PokerDeck
 {
-    private:
-        Card cards[NUMINDECK];
-    public:
-        void shuffle();
-        void deal();
-}
+private:
+	std::vector<Card*> shuffledCards;
+public:
+	Card cards[NUMINDECK];
+	PokerDeck();
+	~PokerDeck();
+	void shuffle();
+	void deal(Hand hands[], int numHands);
+	void printDeck();
+};
 
 #endif
